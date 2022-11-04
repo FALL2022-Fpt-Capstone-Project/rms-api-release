194cbefb1c997595858d8d4bfd1785356c14cbd3
thanhpdhe141032 <85785895+thanhpdhe141032@users.noreply.github.com>
2022-11-04 22:52:50 +0700

    fix bug and api get list room

--- src/main/java/vn/com/fpt/controller/manager/RoomController.java
+++ src/main/java/vn/com/fpt/controller/manager/RoomController.java
@@ -1,14 +1,17 @@
 package vn.com.fpt.controller.manager;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import vn.com.fpt.common.response.AppResponse;
 import vn.com.fpt.common.response.BaseResponse;
 import vn.com.fpt.responses.RoomsResponse;
+import vn.com.fpt.service.rooms.RoomService;
+
+import java.util.List;
 
 import static vn.com.fpt.configs.AppConfigs.*;
 
@@ -20,9 +23,14 @@ import static vn.com.fpt.configs.AppConfigs.*;
 public class RoomController {
     public static final String PATH = V1_PATH + MANAGER_PATH + ROOM_PATH;
 
-    @GetMapping("")
-    public ResponseEntity<BaseResponse<RoomsResponse>> room(){
-        // TODO
-        return null;
+    private final RoomService roomService;
+
+    @GetMapping("/")
+    @Operation(summary = "Danh sách các phòng")
+    public ResponseEntity<BaseResponse<List<RoomsResponse>>> room(@RequestParam(required = false) Long groupId,
+                                                                  @RequestParam(required = false) String name,
+                                                                  @RequestParam(required = false) Long floor,
+                                                                  @RequestParam(required = false, defaultValue = "null") Boolean available){
+        return AppResponse.success(roomService.listRoom(groupId, floor, available, name));
     }
 }
