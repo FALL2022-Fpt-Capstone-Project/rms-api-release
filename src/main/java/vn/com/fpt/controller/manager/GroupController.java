5ef6dc5dbd8aac0fad778bbb703480932c9dc3e6
ADMIN\Admin <thanhpdhe141032@gmail.com>
2022-11-21 01:48:18 +0700

    add api preview

--- src/main/java/vn/com/fpt/controller/manager/GroupController.java
+++ src/main/java/vn/com/fpt/controller/manager/GroupController.java
@@ -6,15 +6,19 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import vn.com.fpt.common.response.AppResponse;
 import vn.com.fpt.common.response.BaseResponse;
 import vn.com.fpt.common.utils.Operator;
+import vn.com.fpt.entity.RoomGroups;
+import vn.com.fpt.entity.Rooms;
 import vn.com.fpt.requests.AddGroupRequest;
 import vn.com.fpt.responses.GroupResponse;
 import vn.com.fpt.service.group.GroupService;
 import vn.com.fpt.service.group.GroupServiceImpl;
+import vn.com.fpt.service.rooms.RoomService;
 
 import java.util.List;
 
@@ -29,6 +33,8 @@ public class GroupController {
     public static final String PATH = V1_PATH + MANAGER_PATH + GROUP_PATH;
 
     private final GroupService groupService;
+
+    private final RoomService roomService;
 
     @GetMapping("/{groupId}")
     @Operation(summary = "Lấy thông tin của nhóm phòng theo id")
@@ -47,4 +53,19 @@ public class GroupController {
     public ResponseEntity<BaseResponse<Object>> add(@RequestBody AddGroupRequest request) {
         return AppResponse.success(groupService.add(request, Operator.operator()));
     }
+
+    @PostMapping("/preview")
+    @Operation(summary = "Xem trước kết quả tự động tạo mới các phòng")
+    public ResponseEntity<BaseResponse<List<Rooms>>> preview(@RequestBody @NotNull AddGroupRequest request) {
+        return AppResponse.success(roomService.previewGenerateRoom(
+                request.getTotalRoomPerFloor(),
+                request.getTotalFloor(),
+                request.getRoomLimitedPeople(),
+                request.getRoomPrice(),
+                request.getRoomArea(),
+                request.getRoomNameConvention(),
+                Operator.operator())
+        );
+    }
+
 }
