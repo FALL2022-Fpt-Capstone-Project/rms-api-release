71642002d0ebfcd37577cfaca05f74ca60e09803
ADMIN\Admin <thanhpdhe141032@gmail.com>
2022-12-11 19:50:41 +0700

    fix deploy

--- src/main/java/vn/com/fpt/requests/PreviewAddBillRequest.java
+++ src/main/java/vn/com/fpt/requests/PreviewAddBillRequest.java
@@ -2,17 +2,13 @@ package vn.com.fpt.requests;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import vn.com.fpt.responses.PreviewAddBillResponse;
 
 
 @Getter
 @Setter
-@AllArgsConstructor
-@RequiredArgsConstructor
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class PreviewAddBillRequest extends PreviewAddBillResponse {
 }
